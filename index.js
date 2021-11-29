72a723eb50cfb1715a273302119d7acd8654a9bd
Andrey <info@shkarin-andrey.ru>
2021-11-30 00:17:36 +0300

    First commit (Node, Express)

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+const cardRoutes = require('./routes/card')
+
+const app = express()
+
+const hbs = exphbs.create( {
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'pages')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
